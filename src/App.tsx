30c54b3ccef4d47ae42c54aa9ba5a37c8df5029d
Mauricio Blum <mauricio.blum@itsector.pt>
2021-02-19 22:16:12 +0000

    added initial app features

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,115 @@
-import React from 'react';
-import logo from './logo.svg';
+import { FormEvent, useState, useRef } from 'react';
 import './App.css';
+import seedrandom from 'seedrandom';
+
+function getRandomInt(min: number, max: number, randomFn: () => number) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(randomFn() * (max - min)) + min;
+}
 
 function App() {
+  const [name, setName] = useState('');
+  const [result, setResult] = useState('');
+  const [resultColor, setResultColor] = useState('black');
+  const resultRef = useRef<HTMLHeadingElement>(null);
+
+  const sortHouse = (e: FormEvent) => {
+    e.preventDefault();
+
+    if (name.length <= 1 || name.match(/^[0-9]+$/) !== null) {
+      alert('Digite um nome válido!');
+      return;
+    }
+
+    const random = seedrandom(name);
+    const houseNumber = getRandomInt(1, 4, random);
+    const yourHouseIs = 'Sua casa é ';
+
+    setResultColor('black');
+    setResult('O chapéu está pensando.');
+
+    setTimeout(() => {
+      switch (houseNumber) {
+        case 1:
+          setResult(yourHouseIs + 'Grifinória');
+          setResultColor('red');
+          break;
+        case 2:
+          setResult(yourHouseIs + 'Sonserina');
+          setResultColor('green');
+          break;
+        case 3:
+          setResult(yourHouseIs + 'Corvinal');
+          setResultColor('orange');
+          break;
+        case 4:
+          setResult(yourHouseIs + 'Lufa Lufa');
+          setResultColor('blue');
+          break;
+        default:
+          setResult(yourHouseIs + 'Lufa Lufa');
+          setResultColor('blue');
+      }
+      resultRef?.current?.scrollIntoView({ behavior: 'smooth' });
+    }, 3000);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <img src="images/Hogwarts.webp" alt="hogwarts crest" height="150" />
+      <h1 className="title">Descubra sua casa em Hogwarts</h1>
+      <form>
+        <input
+          id="name"
+          type="text"
+          name="name"
+          value={name}
+          min={2}
+          className="name"
+          onChange={(e) => setName(e.target.value)}
+          placeholder="Digite o seu nome completo"
+        />
+        <button
+          type="submit"
+          className="copperBtn generalBtn"
+          onClick={sortHouse}
         >
-          Learn React
-        </a>
-      </header>
+          Ver a sua casa
+        </button>
+      </form>
+      <div className={`result ${resultColor}`}>
+        {result && (
+          <>
+            {result.includes('Lufa Lufa') && (
+              <img
+                src="images/Hufflepuff.webp"
+                height="200"
+                alt="house crest"
+              />
+            )}
+            {result.includes('Corvinal') && (
+              <img src="images/Ravenclaw.webp" height="200" alt="house crest" />
+            )}
+            {result.includes('Sonserina') && (
+              <img src="images/Slytherin.webp" height="200" alt="house crest" />
+            )}
+            {result.includes('Grifinória') && (
+              <img
+                src="images/Gryffindor.webp"
+                height="200"
+                alt="house crest"
+              />
+            )}
+            <h2
+              ref={resultRef}
+              className={result.includes('pensando') ? 'appendMovingDots' : ''}
+            >
+              {result}
+            </h2>
+          </>
+        )}
+      </div>
     </div>
   );
 }
